76ba785611f4a81b65c8d99695b0951d8eb11b4c
steven_hert <108745586+steven3002@users.noreply.github.com>
2024-09-09 12:44:43 +0100

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 #![cfg_attr(not(any(feature = "export-abi", test)), no_main)]
 // written by Steven Hert, omo this code needs to be formated after the competition
-#[global_allocator]
-static ALLOC: mini_alloc::MiniAlloc = mini_alloc::MiniAlloc::INIT;
+// #[global_allocator]
+// static ALLOC: mini_alloc::MiniAlloc = mini_alloc::MiniAlloc::INIT;
 extern crate alloc;
 use stylus_sdk::{ alloy_primitives::U256, prelude::*, stylus_proc::entrypoint };
 
@@ -29,9 +29,9 @@ sol_storage! {
     }
 }
 
-#[external]
+#[public]
 impl Game {
-    pub fn new(&mut self) -> String {
+    pub fn new(&mut self) {
         let word = "antidisestablishmentarianism".to_string();
         let hint =
             "opposition to withdrawing state support from an established institution,
@@ -39,59 +39,67 @@ impl Game {
             religious context,
             political term,
             long word".to_string();
-
+        let default_x = "".to_string();
         // omo this is self explanatry
         self.word.set_str(word);
         self.hint.set_str(hint);
-        self.guessed.set_str("");
+        self.guessed.set_str(default_x.clone());
         self.turn.set(U256::from(1));
         self.game_active.set(false);
-        self.player1.name.set_str("");
-        self.player1.name_id.set_str("");
+
+        self.player1.name.set_str(default_x.clone());
+        self.player1.name_id.set_str(default_x.clone());
         self.player1.score.set(U256::from(0));
         self.player1.turn.set(U256::from(1));
-        self.player2.name.set_str("");
-        self.player2.name_id.set_str("");
+
+        self.player2.name.set_str(default_x.clone());
+        self.player2.name_id.set_str(default_x.clone());
         self.player2.score.set(U256::from(0));
-        self.player3.name.set_str("");
-        self.player3.name_id.set_str("");
+        self.player2.turn.set(U256::from(2));
+
+        self.player3.name.set_str(default_x.clone());
+        self.player3.name_id.set_str(default_x.clone());
         self.player3.score.set(U256::from(0));
-        return "Machine new state".to_string();
+        self.player3.turn.set(U256::from(3));
     }
 
-    pub fn add_player(&mut self, name: String) -> String {
+    pub fn add_player(&mut self, name: String) {
         if self.game_active.get() {
-            return String::from("Game has started");
+            return;
         }
-        if self.player1.name.get_string() == String::from("") {
+        if self.player1.name.get_string().is_empty() {
             self.player1.name.set_str(name);
             let user_id = format!("{}", msg::sender());
             self.player1.name_id.set_str(user_id);
-            return String::from("Player1 set");
-        } else if self.player1.name.get_string() == String::from("") {
+            return;
+        } else if self.player2.name.get_string().is_empty() {
             self.player2.name.set_str(name);
             let user_id = format!("{}", msg::sender());
             self.player2.name_id.set_str(user_id);
-            return String::from("Player2 set");
-        } else if self.player1.name.get_string() == String::from("") {
+            return;
+        } else if self.player3.name.get_string().is_empty() {
             self.player3.name.set_str(name);
             let user_id = format!("{}", msg::sender());
             self.player3.name_id.set_str(user_id);
             self.game_active.set(true);
-            return String::from("Player3 set");
+            return;
         } else {
-            return String::from("No more player space");
+            return;
         }
     }
 
     // Guess a letter (called by frontend for each player’s turn)
-    pub fn guess_letter(&mut self, letter: String) -> String {
+    pub fn guess_letter(&mut self, letter: String) {
         if !self.game_active.get() {
-            return String::from("Game over. Start a new game.");
+            return;
         }
 
         if self.my_turn() == 0 {
-            return String::from("It's not your turn.");
+            return;
+        }
+
+        if self.guessed.get_string().contains(letter.as_str()) {
+            return;
         }
 
         // Check if letter is in the word
@@ -122,21 +130,16 @@ impl Game {
             _ => (),
         }
 
-        let character = letter.chars().next();
-
-        match character {
-            Some(c) => self.guessed.get_string().push(c),
-            None => (),
+        let mut guessed = self.guessed.get_string();
+        if let Some(c) = letter.chars().next() {
+            guessed.push(c);
         }
+        self.guessed.set_str(&guessed);
 
         if self.is_word_complete() {
             self.game_active.set(false);
-            let t = self.get_guessed_word();
-            return format!(
-                "Game complete! {} is the winner! word is {}",
-                self.get_winner_name(),
-                t
-            );
+
+            return;
         }
         let turner;
         if self.turn.get() == U256::from(3) {
@@ -146,9 +149,6 @@ impl Game {
         }
         // Change turn to next player
         self.turn.set(turner);
-
-        // Return the guessed state to the frontend
-        self.get_guessed_word()
     }
 
     // Check if the word is fully guessed
@@ -197,12 +197,20 @@ impl Game {
             player3_score
         );
     }
+    pub fn get_hints(&self) -> String {
+        self.hint.get_string()
+    }
+    pub fn get_turn(&self) -> String {
+        let turn_m = self.turn.get();
+        format!("{}", turn_m)
+    }
 }
 
 impl Game {
     pub fn my_turn(&self) -> u8 {
-        // return my turn number if it's my turn
-        // return 0 if it's not my turn
+        // return  if it's my turn
+        // return  if it's not my turn
+
         let user_id = format!("{}", msg::sender());
         if self.player1.name_id.get_string() == user_id {
             if self.player1.turn.get() == self.turn.get() {
